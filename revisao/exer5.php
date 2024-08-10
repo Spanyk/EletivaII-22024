fd569421268a452f55167e01345a21ff6aed9ac5
Spanyk <paulo.augusto62@hotmail.com>
2024-08-10 00:35:49 -0300

    [add] Exercico 5: Calculo de Férias

--- revisao/exer5.php
+++ revisao/exer5.php
@@ -1,30 +1,31 @@
-<?php require('cabecalho.php'); 
+<?php require('cabecalho.php');  ?>
+<div>
+<h1>Exercício 5: Cálculo de Férias</h1>
+    <form method="POST" action="exer5.php">
+      <div class="row">
+        <div class="col">
+          <div class="form-group">
+            <label for="diasTrabalhados">Dias Trabalhados:</label>
+            <input type="number" class="form-control" id="diasTrabalhados" name="diasTrabalhados" required>
+          </div>
+        </div>
+      </div>
+      <div class="row p-2">
+          <button type="submit" class="btn btn-success "> Calcular Dias de Férias</button>
+      </div>
+    </form>
+    <?php
+    if ($_POST) {
+        $diasTrabalhados = (int)$_POST['diasTrabalhados'];
+        $diasFerias = min(floor($diasTrabalhados / 30), 30);
 
-$diasFerias = 0; 
-$diasTrabalhados = 0; 
-
-for ($diasFerias; $diasFerias > $diasTrabalhados;) {
-  for($diasTrabalhados; $diasTrabalhados == 30; $diasTrabalhados++) {
-    if ($diasTrabalhados == 30) {
-      $diasFerias++;
+        echo "<div class='mt-4'>";
+        echo "<h3>Resultados:</h3>";
+        echo "<p>Funcionário trabalhou: <strong>$diasTrabalhados</strong> dias</p>";
+        echo "<p>Dias de férias a que tem direito: <strong>$diasFerias</strong> dias</p>";
+        echo "</div>";
     }
-
-    return $diasFerias;
-  }
-  
-}
-?>
-<!-- Crie uma funcionalidade que calcula a quantidade de dias de férias que um funcionário tem 
-direito com base no tempo de serviço na empresa.
-
-? A regra para cálculo de férias é a seguinte: 
-Para cada 30 dias trabalhados o funcionário pode receber 1 dia de férias; ele pode no máximo 
-tirar 30 dias de férias a partir do total de dias de férias informado.
- -->
-
-<div class="row">
-  <h1>Exercicio 5 </h1>
-  <div class="col">
-    
-  </div>
+    ?>
 </div>
+
+<?php require('rodape.php'); ?>
