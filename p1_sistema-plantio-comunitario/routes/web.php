c4f0ebde5bda3632e714dfc04215707c43a95279
Spanyk <paulo.augusto62@hotmail.com>
2024-10-05 07:50:48 -0300

    alteração arquivo de rotas

--- p1_sistema-plantio-comunitario/routes/web.php
+++ p1_sistema-plantio-comunitario/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\TerrenoController;
+use App\Models\Terreno;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/layouts/app');
 });
+
+
+Route::resource('terreno', TerrenoController::class);
+
+Route::get('/terreno/{id}/edit', [TerrenoController::class, 'edit'])->name('terreno.edit');
+
+Route::post('/terrenos/{terreno}', [TerrenoController::class, 'destroy'])->name('terreno.delete');
