8e89ea84fc381dc7348fcd9538eb0d38e1e2a6bb
Spanyk <paulo.augusto62@hotmail.com>
2024-11-30 06:11:22 -0300

    Atualizando model Plantion

--- p1_sistema-plantio-comunitario/app/Models/Plantio.php
+++ p1_sistema-plantio-comunitario/app/Models/Plantio.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
+use App\Models\Morador;
+use App\Models\Planta;
+use App\Models\Terreno;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Plantio extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'morador_id',
+        'planta_id',
+        'terreno_id',
+        'data_plantio',
+        'data_colheita',
+        'status'
+    ];
+
+    public function morador() {
+        return $this->belongsTo(Morador::class);
+    }
+
+    public function terreno() {
+        return $this->belongsTo(Terreno::class);
+    }
+    public function planta() {
+        return $this->belongsTo(Planta::class);
+    }
 }
