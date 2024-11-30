befc898a7ed3bbf4af93c100ee26e98c8f73460a
Spanyk <paulo.augusto62@hotmail.com>
2024-11-30 06:12:32 -0300

    atualizacao do arquivo de rotas

--- p1_sistema-plantio-comunitario/routes/web.php
+++ p1_sistema-plantio-comunitario/routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\MoradorController;
+use App\Http\Controllers\PlantaController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TerrenoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +19,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('planta', PlantaController::class);
+    Route::resource('terreno', TerrenoController::class);
+    Route::resource('morador', MoradorController::class);
 });
 
 require __DIR__.'/auth.php';
